c2cb124ed430d7d13d66ffedac9151ec7c60ef6e
Bennie <brobinson88@gmail.com>
2018-07-09 08:28:19 -0400

    add safe functions, demonstrate double

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const Maybe = require('crocks/Maybe')
 
-const { inc, upper } = require('./utils')
+const { inc, upper, double } = require('./utils')
 
+const number = val => typeof val === 'number'
+const string = val => typeof val === 'string'
 const safe = (pred, val) => pred(val) ? Maybe.Just(val) : Maybe.Nothing()
 
-const input = safe('number', 2)
+const input = safe(number, 2)
 const result = input.map(inc)
 console.log(result)
 
-const input2 = safe('string', 'bit')
+const input2 = safe(string, 'bit')
 const result2 = input2.map(upper)
 console.log(result2)
+
+const doubleOfInc = n => {
+  const safeNumber = safe(number, n)
+  return safeNumber.map(inc).map(double)
+}
+const result3 = doubleOfInc(2)
+console.log(result3)
--- utils.js
+++ utils.js
@@ -2,7 +2,10 @@ const inc = n => n + 1
 
 const upper = s => s.toUpperCase()
 
+const double = n => n * 2
+
 module.exports = {
   inc,
-  upper
+  upper,
+  double
 }
